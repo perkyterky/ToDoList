1dcfc98594137e10990144f0b5187dc1085638ba
Patrick Tang <meigao101cpw@gmail.com>
2022-04-23 12:56:48 -0400

    done with this project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,57 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
+import userEvent from '@testing-library/user-event';
+afterEach=()=>{
+  localStorage.clear()
+}
+test('Title is Visible', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByRole ("heading", {name: "To-Do List"});
+  expect(title).toBeVisible();
 });
+test('Basic Element Show-up', () => {
+  render(<App/>);
+  const inputField = screen.getByPlaceholderText ("Create a new To-Do")
+  const submitButton = screen.getByRole ("button", {name: "Submit"})
+  expect(inputField).toBeVisible();
+  expect(submitButton).toBeDisabled();
+
+});
+
+test('Adding Value to To-Do List', () => {
+  render(<App/>);
+  const inputField = screen.getByPlaceholderText ("Create a new To-Do")
+  const submitButton = screen.getByRole ("button", {name: "Submit"})
+  userEvent.type(inputField, "Feed the Shark")
+  userEvent.click(submitButton)
+  expect(screen.getByText("Feed the Shark")).toBeVisible()
+});
+
+test("Completing a To-Do item", ()=> {
+  render(<App/>);
+  const inputField = screen.getByPlaceholderText ("Create a new To-Do")
+  const submitButton = screen.getByRole ("button", {name: "Submit"})
+  const toDoList = screen.getByLabelText ("primary-list")
+  const doneList = screen.getByLabelText ("secondary-list")
+  userEvent.type(inputField, "Test for moving items")
+  userEvent.click(submitButton)
+  expect(within(toDoList).getByText("Test for moving items"))
+  userEvent.click(within(toDoList).getByText("Test for moving items"))
+  expect(within(doneList).getByText("Test for moving items"))
+
+})
+
+test('Header for primary and secondary', () => {
+  render(<App/>);
+  const finishedItems = screen.getByRole ("heading", {name: "Finished Items"});
+  const unfinishedItems = screen.getByRole ("heading", {name: "Unfinished Items"});
+  expect(finishedItems).toBeVisible();
+  expect(unfinishedItems).toBeVisible();
+
+
+});
+// when I come to website 
+// and create a to-do item by typing one in
+// and then submit it
+// and then click it, I expect for it to move to the other list
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+  const startingValue = localStorage.getItem("toDoList")
+    ? JSON.parse(localStorage.getItem("toDoList")!!)
+    : [""];
+  const [toDoList, setToDoList] = useState<string[]>(startingValue);
+  const [doneList, setDoneList] = useState<string[]>([]);
+  function addValueToItems(event: React.FormEvent) {
+    event.preventDefault();
+    let newItems = toDoList;
+    newItems.push(value);
+    console.log(value);
+    setToDoList(newItems);
+    setValue("");
+    localStorage.setItem("toDoList", JSON.stringify(newItems));
+  }
+  function moveItem(item: string) {
+    let newItems = toDoList;
+    const index = newItems.indexOf(item);
+    if (index > -1) {
+      newItems.splice(index, 1);
+    }
+    setToDoList(newItems);
+    localStorage.setItem("toDoList", JSON.stringify(newItems));
+    forceUpdate();
+    let newDoneList = doneList;
+    newDoneList.push(item);
+    setDoneList(newDoneList);
+  }
+  let isButtonDisabled = !/[a-z]/i.test(value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack gap = {2} alignItems = "center" >
+      <Typography variant = "h3">To-Do List</Typography>
+      <form onSubmit={addValueToItems}>
+        <TextField size = "small"
+        label = "Create a New To-Do"
+          placeholder="Walk the bed"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        ></TextField>
+
+        <Button variant="contained" size = "large" type="submit" disabled={isButtonDisabled}>
+          Submit
+        </Button>
+      </form>
+      <Typography variant = "h5">Unfinished Items</Typography>
+      <List aria-label="primary-list">
+        {toDoList.map((item, index) => (
+          <ListItem onClick={() => moveItem(item)}>{index + 1}. {item}</ListItem>
+        ))}
+      </List>
+      <Typography variant = "h5">Finished Items</Typography>
+      <List aria-label="secondary-list">
+        {doneList.map((item, index) => (
+          <ListItem>{index + 1}. {item}</ListItem>
+        ))}
+      </List>
+    </Stack>
   );
 }
 
